3678f340becfaeccd2e558b58a09e8d668eb50d3
Jinping Wu <jinping.wu@verisilicon.com>
2023-06-17 15:04:44 +0800

    add mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,37 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    @R2
+    M=0
+    @i
+    M=0
+(LOOP)
+    @i
+    D=M //get i
+
+    @R1
+    D=D-M //get i - r1
+
+    @END
+    D;JEQ //if i - r1 == 0, jump to end
+
+    @i
+    D=M+1 //d = i + 1
+    M=D //i = i + 1
+
+    @R0
+    D=M //d = r0
+    @R2
+    D=D+M //d= r2 + r0
+    M=D // r2 = r2 + r0
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+
+
